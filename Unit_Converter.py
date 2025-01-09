eac084e9fd1ec877b1f371cf4b2bca5a4f737675
Muhammad Kamil <muhammadkamil0.988@gmail.com>
2025-01-09 18:01:47 +0500

    Adding function for weight

--- Unit_Converter.py
+++ Unit_Converter.py
@@ -17,6 +17,21 @@ def Height_Conversion():
         print("Invalid input.")
 
 
+def Weight_Conversion():
+    unit_choice = input("What unit do you want to convert from: ").strip().lower()
+    try:
+        if unit_choice == "lbs":
+            Weight = float(input("What is your weight in lbs: ").strip())
+            print(f"Your weight in kgs is: {WeightInKg(Weight):.2f}")
+        elif unit_choice == "kgs":
+            Weight = float(input("What is your weight in kgs: ").strip())
+            print(f"Your weight in lbs is: {WeightInLbs(Weight):.2f}")
+        else:
+            print("Unsupported unit choice.")
+    except ValueError:
+        print("Invalid input.")
+
+
 
 
 
