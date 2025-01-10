bef7d58443f953864fa27c111d127d6067bf04df
Muhammad Kamil <muhammadkamil0.988@gmail.com>
2025-01-10 15:34:15 +0500

    Initial commit for a simple calculator

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+def  calculator():
+    print("Welcome!")
+    print("To start press:")
+    print("1. Multiplication")
+    print("2. Division")
+    print("3. Addition")
+    print("4. Subtraction")
+
+    
