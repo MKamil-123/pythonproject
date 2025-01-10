58d0849acc44d5a68a83839011b0a28d6d4afda4
Muhammad Kamil <muhammadkamil0.988@gmail.com>
2025-01-10 15:36:10 +0500

    Finished the simple calculator

--- Calculator.py
+++ Calculator.py
@@ -6,4 +6,24 @@ def  calculator():
     print("3. Addition")
     print("4. Subtraction")
 
-    
+    choice = input("Enter the corresponding number for your desired choice (1,2,3,4): ")
+
+    num1 = input("Enter the first number: ")
+    num2 = input("Enter the second number: ")
+
+    if choice == "1":
+        print(f"The result of {num1}*{num2} is {num1 * num2}")
+    elif choice == "2":
+        if num2 != 0:
+            print(f"The result of {num1}/{num2} is {num1 / num2}")
+        else:
+            print("You can't divide by zero.")
+    elif choice == "3":
+            print(f"The result of {num1} + {num2} is {num1 + num2}")
+    elif choice == "4":
+        print(f"The result of {num1} - {num2} is {num1 - num2}")
+    else:
+        print("Invalid choice.")
+
+
+calculator()
